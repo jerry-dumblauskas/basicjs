bb547e0dc9709fe21f0e1cbb8fe4833184a9b146
jerry-dumblauskas <jdumblauskas@gmail.com>
2016-04-02 17:00:45 -0500

    babel working

--- public/dist/test.js
+++ public/dist/test.js
@@ -1,13 +1,38 @@
 'use strict';
 // es6 feature: block-scoped "let" declaration
 
-const sentences = [{ subject: 'JavaScript', verb: 'is', object: 'great' }, { subject: 'Elephants', verb: 'are', object: 'large' }];
+var sentences = [{ subject: 'JavaScript', verb: 'is', object: 'great' }, { subject: 'Elephants', verb: 'are', object: 'large' }];
 // es6 feature: object destructuring
-function say({ subject, verb, object }) {
+function say(_ref) {
+    var subject = _ref.subject;
+    var verb = _ref.verb;
+    var object = _ref.object;
+
     // es6 feature: template strings
-    console.log(`${ subject } ${ verb } ${ object }`);
+    console.log(subject + ' ' + verb + ' ' + object);
 }
 // es6 feature: for..of
-for (let s of sentences) {
-    say(s);
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = sentences[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var s = _step.value;
+
+        say(s);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
 }
